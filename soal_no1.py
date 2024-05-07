b2daedb9b946a32134048dfaeea0fd98f1935887
idris <149251188+marzuki23@users.noreply.github.com>
2024-05-07 16:32:23 +0700

    Add files via upload

---
+++ soal_no1.py
@@ -0,0 +1,6 @@
+angka1 = float(input("Masukkan bilangan pertama: "))
+angka2 = float(input("Masukkan bilangan kedua: "))
+    
+print("Penjumlahan:", angka1 + angka2)
+print("Pengurangan:", angka1 - angka2)
+print("Modulus:", angka1 % angka2)
